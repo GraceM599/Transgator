466566dfca28c3162a61491b53d540d6a303c82d
Grace Mihiral <mihiralgrace@gmail.com>
2026-03-14 12:34:25 -0400

    Hash Map prefix search and return complete

--- src/HashMap.h
+++ src/HashMap.h
@@ -4,6 +4,8 @@
 #include "dictionary.h"
 #include <fstream>
 #include <iostream>
+#include <queue>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -111,9 +113,63 @@ public:
             }
 
         }
-        return "Not found in table";
+        return "Not found!";
     }
-    //std::prefix_search
+    Slot search_Slot(std::string en) {
+        int loc = hash(en) % table.size(); // ensure within bounds
+
+        for (int i = 0; i < table.size(); ++i) {
+            int index = (loc + i) % table.size();
+
+            if (table[index].en == en) {
+                //key matches
+                return table[index];
+            }
+
+        }
+        return Slot();
+    }
+
+    std::vector<std::tuple<std::string, std::string>> prefixSearch(std::string en) {
+        //take key and add 1-3 characters to the end to get all valid words up to 3 letters away from prefix
+        //std::string search and add to final vector<tuple> if return != null
+        std::vector<std::string> chars = {
+            "", "a","b","c","d","e","f","g","h","i","j","k","l","m",
+            "n","o","p","q","r","s","t","u","v","w","x","y","z"
+        };
+        std::string word;
+        Slot t;
+        auto f = [](const Slot& a, const Slot& b) {
+            return a.freq < b.freq;
+        };
+        std::priority_queue<Slot, std::vector<Slot>, decltype(f)> total(f);
+        std::unordered_set<std::string> seen;
+
+        for (int i=0; i<chars.size(); ++i) {
+            for (int j = 0; j<chars.size(); ++j) {
+                for (int k = 0; k<chars.size(); ++k) {
+                    word = en + chars[i] + chars[j] + chars[k];
+                    if (seen.count(word)) {
+                        continue;
+                    }
+                    seen.insert(word);
+                    t = search_Slot(word);
+                    if (t.freq != 0) {
+                        total.push(t);
+                    }
+                }
+            }
+        }
+        std::vector<std::tuple<std::string, std::string>> result;
+        int limit = std::min(10, (int)total.size());
+        for (int i=0; i<limit; ++i) {
+            auto curr = total.top();
+            total.pop();
+            result.push_back({curr.en, curr.translation});
+        }
+        return result;
+    }
+
 
 };
 
